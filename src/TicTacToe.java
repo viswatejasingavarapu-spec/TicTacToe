f5047de1e5ac3eefed05a259f24f628c4c3be8c0
viswatejasingavarapu-spec <viswatejasingavarapu@gmail.com>
2026-04-29 15:03:32 +0530

    Update TicTacToe.java

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,35 +1,27 @@
 public class TicTacToe {
-    public static boolean checkWinner(char[][] board, char symbol) {
+    public static boolean isBoardFull(char[][] board) {
         for (int i = 0; i < 3; i++) {
-            if (board[i][0] == symbol && board[i][1] == symbol && board[i][2] == symbol) {
-                return true;
+            for (int j = 0; j < 3; j++) {
+                if (board[i][j] == '-') {
+                    return false;
+                }
             }
         }
-        for (int j = 0; j < 3; j++) {
-            if (board[0][j] == symbol && board[1][j] == symbol && board[2][j] == symbol) {
-                return true;
-            }
-        }
-        if (board[0][0] == symbol && board[1][1] == symbol && board[2][2] == symbol) {
-            return true;
-        }
-        if (board[0][2] == symbol && board[1][1] == symbol && board[2][0] == symbol) {
-            return true;
-        }
-        return false;
+        return true;
+    }
+    public static boolean checkDraw(char[][] board) {
+        return isBoardFull(board);
     }
     public static void main(String[] args) {
         char[][] board = {
-            {'X', 'X', 'X'},
-            {'-', 'O', '-'},
-            {'O', '-', '-'}
+            {'X', 'O', 'X'},
+            {'X', 'O', 'O'},
+            {'O', 'X', 'X'}
         };
-        if (checkWinner(board, 'X')) {
-            System.out.println("Player X wins!");
-        } else if (checkWinner(board, 'O')) {
-            System.out.println("Player O wins!");
+        if (checkDraw(board)) {
+            System.out.println("Game is a Draw!");
         } else {
-            System.out.println("No winner yet.");
+            System.out.println("Moves still available.");
         }
     }
 }
