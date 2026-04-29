09ca9dddf64cc6358fdb3d8458bcb8f308d82a59
viswatejasingavarapu-spec <viswatejasingavarapu@gmail.com>
2026-04-29 14:18:28 +0530

    Update TicTacToe.java

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,13 +1,13 @@
-import java.util.Scanner;
 public class TicTacToe {
-    public static int getUserInput() {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a slot number (1-9): ");
-        int slot = sc.nextInt();
-        return slot; // return entered value
+    public static int[] convertSlotToIndex(int slot) {
+        int row = (slot - 1) / 3;
+        int col = (slot - 1) % 3;
+        return new int[]{row, col};
     }
     public static void main(String[] args) {
-        int userChoice = getUserInput();
-        System.out.println("You selected slot: " + userChoice);
+        int slot = 5;
+        int[] position = convertSlotToIndex(slot);
+        System.out.println("Row: " + position[0]);
+        System.out.println("Column: " + position[1]);
     }
 }
