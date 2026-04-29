6c6b126c4b3d8f3ac98499549a97f9891c18ad2b
viswatejasingavarapu-spec <viswatejasingavarapu@gmail.com>
2026-04-29 12:56:22 +0530

    Update TicTacToe.java

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,5 +1,20 @@
 public class TicTacToe {
+    public static void displayBoard() {
+        char[][] board = new char[3][3];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                board[i][j] = '-';
+            }
+        }
+        System.out.println("Tic-Tac-Toe Board:\n");
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
     public static void main(String[] args) {
-
+        displayBoard();
     }
 }
