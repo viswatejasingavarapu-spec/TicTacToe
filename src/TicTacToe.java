f35fe76d6b547163b18e4a26458bc688eb6005da
viswatejasingavarapu-spec <viswatejasingavarapu@gmail.com>
2026-04-29 14:33:01 +0530

    Update TicTacToe.java

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,6 +1,38 @@
+import java.util.Random;
 public class TicTacToe {
+    public static int[] convertSlotToIndex(int slot) {
+        int row = (slot - 1) / 3;
+        int col = (slot - 1) % 3;
+        return new int[]{row, col};
+    }
+    public static boolean isValidMove(char[][] board, int row, int col) {
+        if (row < 0 || row > 2 || col < 0 || col > 2) {
+            return false;
+        }
+        if (board[row][col] != '-') {
+            return false;
+        }
+        return true;
+    }
     public static void placeSymbol(char[][] board, int row, int col, char symbol) {
         board[row][col] = symbol;
+    }
+    public static void computerMove(char[][] board, char symbol) {
+        Random rand = new Random();
+        int slot;
+        int row;
+        int col;
+        while (true) {
+            slot = rand.nextInt(9) + 1;
+            int[] pos = convertSlotToIndex(slot);
+            row = pos[0];
+            col = pos[1];
+
+            if (isValidMove(board, row, col)) {
+                placeSymbol(board, row, col, symbol);
+                break;
+            }
+        }
     }
     public static void main(String[] args) {
         char[][] board = {
@@ -8,10 +40,7 @@ public class TicTacToe {
             {'-', '-', '-'},
             {'-', '-', '-'}
         };
-        int row = 1;
-        int col = 1;
-        char symbol = 'X';
-        placeSymbol(board, row, col, symbol);
+        computerMove(board, 'O');
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 System.out.print(board[i][j] + " ");
