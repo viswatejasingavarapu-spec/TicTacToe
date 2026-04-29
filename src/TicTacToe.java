d664316c42373d4ef13ca72278b2f7db7523df30
viswatejasingavarapu-spec <viswatejasingavarapu@gmail.com>
2026-04-29 14:10:52 +0530

    Update TicTacToe.java

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,20 +1,26 @@
+import java.util.Random;
 public class TicTacToe {
-    public static void displayBoard() {
-        char[][] board = new char[3][3];
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                board[i][j] = '-';
-            }
+    static char player1Symbol;
+    static char player2Symbol;
+    static int currentPlayer;
+    public static void tossAndAssign() {
+        Random rand = new Random();
+        int toss = rand.nextInt(2);
+        if (toss == 0) {
+            currentPlayer = 1;
+            player1Symbol = 'X';
+            player2Symbol = 'O';
+        } else {
+            currentPlayer = 2;
+            player2Symbol = 'X';
+            player1Symbol = 'O';
         }
-        System.out.println("Tic-Tac-Toe Board:\n");
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                System.out.print(board[i][j] + " ");
-            }
-            System.out.println();
-        }
+        System.out.println("Toss Result:");
+        System.out.println("Player " + currentPlayer + " starts first!");
+        System.out.println("Player 1 Symbol: " + player1Symbol);
+        System.out.println("Player 2 Symbol: " + player2Symbol);
     }
     public static void main(String[] args) {
-        displayBoard();
+        tossAndAssign();
     }
 }
